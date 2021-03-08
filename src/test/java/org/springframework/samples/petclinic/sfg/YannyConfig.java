943e1d3cd9897655a856af34006cf93f00e84271
André Félix <andrefialhofelix@gmail.com>
2021-03-08 16:56:26 +0000

    - Section 13, done until 146 -> Assignment Yanny JUnit4 Test with Spring context

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/YannyConfig.java
@@ -0,0 +1,22 @@
+package org.springframework.samples.petclinic.sfg;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Annotating a class with the @Configuration indicates that the class can be used
+ * by the Spring IoC container as a source of bean definitions
+ */
+@Configuration
+public class YannyConfig {
+
+    /**
+     * The @Bean annotation tells Spring that a method annotated with @Bean will return
+     * an object that should be registered as a bean in the Spring application context.
+     * @return YannyWordProducer
+     */
+    @Bean
+    YannyWordProducer yarnWordProducer() {
+        return new YannyWordProducer();
+    }
+}
